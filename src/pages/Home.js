c1573bc1de06111e259cd5282b5a85ff50f7b781
vinaykumar-zine <vkumarzine@gmail.com>
2024-04-25 13:20:26 +0530

    pushhing the final project

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NotesArea from '../components/NotesArea'
+
+
+const Home = () => {
+  return (
+    <div style={{display: 'flex'}}>
+      <NotesArea/>
+
+    </div>
+  )
+}
+
+export default Home
